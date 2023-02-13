d7b367eb362488c28ffc4e834db162bd8e4f4b96
EliasDeHondt <Elias.dehondt@outlook.com>
2023-02-13 21:17:25 +0100

    Commit van de M1.2_v1

---
+++ script/map.js
@@ -0,0 +1,8 @@
+/*
+ * Van Elias De Hondt
+ * https://eliasdh.com
+*/
+/* Contact */
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+/* Contact */
