f1c044edbe4d5b68d03861f0ac75fcf6b132913d
EliasDeHondt <Elias.dehondt@outlook.com>
2023-02-28 20:03:09 +0100

    Commit CSS AND MAP

--- script/map.js
+++
@@ -1,8 +0,0 @@
-/*
- * Van Elias De Hondt
- * https://eliasdh.com
-*/
-/* Contact */
-var map = L.map('map').setView([51.505, -0.09], 13);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-/* Contact */
