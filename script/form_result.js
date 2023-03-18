16d3392b518862249c76fbfd8567a2ed2ccee107
EliasDeHondt <Elias.dehondt@outlook.com>
2023-03-18 20:38:22 +0100

    Commit CSS #

--- script/form_result.js
+++ script/form_result.js
@@ -7,16 +7,6 @@
 const url = new URL(window.location);
 const params = new URLSearchParams(url.search);
 
-/*
-const name_form = params.get('name-form');
-const email_form = params.get('email-form');
-const leveringsadres = params.get('leveringsadres');
-const product = params.get('product');
-const kleur = params.get('kleur');
-const grootte = params.get('grootte');
-const option = params.get('option');
-*/
-
 let html = '<table>';
 
 for (const [key, value] of params.entries()) {
